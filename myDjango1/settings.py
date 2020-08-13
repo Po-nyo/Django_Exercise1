cda923a24509f274adbc1b92319820f292039be5
WooKyungjin <wkj1103@naver.com>
2020-08-13 23:48:12 +0900

    refactor

--- myDjango1/settings.py
+++ myDjango1/settings.py
@@ -15,12 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Secrets
+
+secrets = open(os.path.abspath('secrets.txt'), encoding='utf-8')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0pqwl-@2u71+uu^4b+*zp_e#vvg-+#9j51%tg3li1*j_-&-x&-'
+SECRET_KEY = secrets.readline().rstrip('\n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,7 +127,6 @@ STATIC_URL = '/static/'
 
 
 # Email Setting
-secrets = open(os.path.abspath('secrets.txt'), encoding='utf-8')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
